c573ba51adfcd2cd79f692f73d20ed44daabd4ed
Grace <gracedes@bu.edu>
2024-09-08 18:25:30 -0400

    implemented basic coyote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
+var ctimer = 0.0
+var ctime = false
+@export var ctime_max = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +14,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	ctimef(delta)
+	if Input.is_action_just_pressed("ui_accept") and ctime:
 		velocity.y = JUMP_VELOCITY
+		ctimer = ctime_max
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +28,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func ctimef(delta: float):
+	if is_on_floor() and ctimer != 0.0:
+		ctimer = 0.0
+	else:
+		ctimer += delta
+	ctime = ctimer < ctime_max
